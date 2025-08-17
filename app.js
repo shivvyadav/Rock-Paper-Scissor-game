7871d699103d067643c286764084c425786766ce
Shiv Yadav <shivyadav0014@gmail.com>
2025-08-17 21:13:31 +0545

    start adding js code

---
+++ app.js
@@ -0,0 +1,8 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
