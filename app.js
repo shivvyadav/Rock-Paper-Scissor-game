809a18c817571eaca14d6afe2c13e7324d77f9e1
Shiv Yadav <shivyadav0014@gmail.com>
2025-08-26 23:09:52 +0545

    add choice logic

--- app.js
+++ app.js
@@ -31,3 +31,26 @@ const showWinner = (userWin, userChoice, computerChoice) => {
     msg.style.backgroundColor = "red";
   }
 };
+
+const playGame = (userChoice) => {
+  //Generate computer choice
+  const compChoice = genComputerChoice();
+
+  if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      //scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      //rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      //rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
